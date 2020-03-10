9051ccd76b06151c7c72192621541a79ab860ebd
Colby Barthelmess <colbybarthelmess@waypointbuilding.com>
2020-03-10 11:46:22 -0700

    start tests for guessed words

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 
 import Congrats from './Congrats';
 import { findByTestAttr, checkProps } from '../test/testUtils'
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false };
 
 /**
- * Factory function to create a ShallowWrapper for the App component
+ * Factory function to create a ShallowWrapper for the Congrats component
  * @function setup
  * @param {object} props - Component props specific to this setup
  * @param {object} sttate = Initial state for setup
@@ -42,7 +40,7 @@ test('renders non-empty congrats message when `success` is true', () => {
     expect(message.text().length).not.toBe(0);
 });
 
-test('does not throww warning with expected props', () => {
+test('does not throw warning with expected props', () => {
     const expectedProps = { success: false };
     checkProps(Congrats, expectedProps);
 })
