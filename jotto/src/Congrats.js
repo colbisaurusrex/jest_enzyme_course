972a28d7d134c572b816201034a1cae0d33e0e25
Colby Barthelmess <colbybarthelmess@waypointbuilding.com>
2020-03-13 12:31:05 -0700

    fix Congrats UI

--- jotto/src/Congrats.js
+++ jotto/src/Congrats.js
@@ -8,15 +8,19 @@ import PropTypes from 'prop-types';
  */
 
 const Congrats = (props) => {
-    return (
-        <div data-test="component-congrats" className="alert alert-success">
-            {props.success &&
-                <span data-test="congrats-message">
-                    Congratulations! You guessed the word!
-                </span>
-            }
-        </div>
-    );
+    if(props.success) {
+        return (
+            <div data-test="component-congrats" className="alert alert-success">
+                {props.success &&
+                    <span data-test="congrats-message">
+                        Congratulations! You guessed the word!
+                    </span>
+                }
+            </div>
+        );
+    } else {
+        return <div  data-test="component-congrats" />
+    }
 };
 
 Congrats.propTypes = {
